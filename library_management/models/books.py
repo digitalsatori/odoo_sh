81c6ae979b6d9c359216b9c696928e084f2871c1
Tony Gu <tony@openerp.cn>
2019-01-03 14:19:08 +0800

    add widget many2many support

--- library_management/models/books.py
+++ library_management/models/books.py
@@ -1,14 +1,18 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import models, fields
+
 
 class BookTags(models.Model):
+    _description = "Tags for library books"
     _name = 'library_management.booktags'
 
     tagname = fields.Char()
     color_index = fields.Integer()
 
+
 class Books(models.Model):
+    _description = "Library Books"
     _name = 'library_management.books'
 
     name = fields.Char()
